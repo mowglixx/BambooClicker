a4e2e8700ec6d61dd1821df253972c6201e90d93
unknown <jakubkarols@gmail.com>
2023-01-30 18:36:36 +0000

    New features: - Made a button for buying new plants - Added counter for plants you have - Added basic increment for every plant you have - Made sure that all values get saved and displayed properly when reloaded Few quality life improvments as well

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bambooImage = document.querySelector("#bamboo")
 const bambooScoreDisplay = document.querySelector("#score")
 const upgradeCost = document.querySelector(".cost")
 const buyButton = document.querySelector(".buy")
-
+const plantAmountDisplay = document.querySelector(".amount")
+let plantAmount = JSON.parse(localStorage.getItem("plant")) 
 //Updating score on refresh from localStorage
 
 bambooScoreDisplay.innerHTML = localStorage.getItem("score")
@@ -16,13 +17,16 @@ const addScore = () => {
     localStorage.setItem("score", bambooScore)
 }
 
-//Basic score remover to buy things
+//Basic buying function
 
-const removeScore = () => {
+const buyPlant = () => {
     if (bambooScore >= parseInt(upgradeCost.textContent)) {
     bambooScore = bambooScore - parseInt(upgradeCost.textContent)
     bambooScoreDisplay.innerHTML = bambooScore
     localStorage.setItem("score", bambooScore)
+    plantAmount = plantAmount + 1
+    localStorage.setItem("plant", plantAmount)
+    plantAmountDisplay.innerHTML = plantAmount
     }
     else {
         alert("You need more bamboo!")
@@ -32,10 +36,11 @@ const removeScore = () => {
 //Adding bamboo over time, 1000ms = 1s
 
 const bambooOverTime = () => {
-    
+    bambooScore = Math.round(bambooScore + (plantAmount * 0.1))
+    bambooScoreDisplay.innerHTML = bambooScore
 }
 
 setInterval(bambooOverTime, 1000)
 
 bambooImage.addEventListener("click", addScore)
-buyButton.addEventListener("click", removeScore)
+buyButton.addEventListener("click", buyPlant)
