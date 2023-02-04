8e07c867453bca993568bafb4a6814f2d2e66dce
unknown <jakubkarols@gmail.com>
2023-02-04 18:56:07 +0000

    Big content update. Version 0.1 finally out Added at least 1 upgrade per building Added achievements Added picture for eveyrthing that is in game so far Added a reset button

--- app.js
+++ app.js
@@ -13,47 +13,7 @@ const plantAmountDisplay = document.querySelector(".plantAmount")
 const shopContainer = document.querySelector("#shopContainer")
 const upgradeContainer = document.querySelector("#upgradeContainer")
 const achievementContainer = document.querySelector("#achievementContainer")
-//Updating score on refresh from localStorage
-
-//bambooScoreDisplay.innerHTML = localStorage.getItem("score")
-//plantAmountDisplay.innerHTML = localStorage.getItem("plant")
-
-//Adding score to counter
-
-// const addScore = () => {
-//     bambooScore = bambooScore + clickingPower
-//     bambooScoreDisplay.innerHTML = bambooScore
-//     localStorage.setItem("score", bambooScore)
-// }
-
-//Basic buying function
-
-// const buyPlant = () => {
-//     if (bambooScore >= parseInt(upgradeCost.textContent)) {
-//     bambooScore = bambooScore - parseInt(upgradeCost.textContent)
-//     bambooScoreDisplay.innerHTML = bambooScore
-//     localStorage.setItem("score", bambooScore)
-//     plantAmount = plantAmount + 1
-//     localStorage.setItem("plant", plantAmount)
-//     plantAmountDisplay.innerHTML = plantAmount
-//     }
-//     else {
-//         alert("You need more bamboo!")
-//     }
-// }
-
-//Adding bamboo over time, 1000ms = 1s
-
-// const bambooOverTime = () => {
-//     bambooScore = bambooScore + plantAmount * 0.1
-//     bambooScoreDisplay.innerHTML = Math.round(bambooScore * 10) / 10
-//     bambooPerSecondDisplay.innerHTML = plantAmount * 0.1
-// }
-
-// setInterval(bambooOverTime, 1000)
-
-//bambooImage.addEventListener("click", addScore)
-//buyButton.addEventListener("click", buyPlant)
+const achievementPopUp = document.querySelector("#achievementPopUp")
 
 //Game object - Contains all important variables and socre functions
 
@@ -90,7 +50,7 @@ const display = {
     udpateShop: function(){
         shopContainer.innerHTML = ""
         for (i=0; i < building.name.length; i++){
-            shopContainer.innerHTML += `<table id="${building.name[i]}" class="unselectable" index="${i}"><tr index="${i}"><td class="image" index="${i}"><img src="./img/${building.image[i]}" index="${i}"></td><td index="${i}">Bamboo ${building.name[i]}</td><td index="${i}">[<span index="${i}">${building.cost[i]}</span> ${building.price[i]}]:</td><td index="${i}"><span index="${i}">${building.amount[i]}</span></td></tr></table>`
+            shopContainer.innerHTML += `<table id="${building.name[i]}" class="unselectable" index="${i}"><tr index="${i}"><td class="image" index="${i}"><img src="./img/${building.image[i]}" index="${i}"></td><td index="${i}">Bamboo ${building.name[i]}</td><td index="${i}">[<span index="${i}">${building.cost[i]}</span> bamboo]:</td><td index="${i}"><span index="${i}">${building.amount[i]}</span></td></tr></table>`
         }
     },
     updateUpgrades: function(){
@@ -103,6 +63,17 @@ const display = {
                 else if (upgrade.type[i] == "click" && game.totalClicks >= upgrade.requirement[i]){
                     upgradeContainer.innerHTML += `<img src="./img/${upgrade.image[i]}" title="${upgrade.name[i]} &#10; ${upgrade.description[i]} &#10; ${upgrade.cost[i]} bamboo" index="${i}">`
                 }
+            }
+        }
+    },
+    updateAchievement: function(){
+        achievementContainer.innerHTML = ""
+        for (i=0; i < achievement.name.length; i++){
+            if (achievement.achieved[i]){
+                achievementContainer.innerHTML += `<img src="./img/${achievement.image[i]}" title="${achievement.name[i]} &#10; ${achievement.description[i]}">`
+            }
+            else {
+                achievementContainer.innerHTML += `<img src="./img/questionmark.png">`
             }
         }
     }
@@ -134,15 +105,15 @@ const building = {
         "system.png"
     ],
     cost: [
-        10,
-        10,
-        10,
-        10,
-        10,
-        10,
-        10,
-        10,
-        10
+        15,
+        100,
+        1100,
+        12000,
+        130000,
+        1400000,
+        20000000,
+        330000000,
+        5100000000
     ],
     amount: [
         0,
@@ -157,14 +128,14 @@ const building = {
     ],
     income: [
         0.1,
-        2,
-        25,
-        300,
-        3200,
-        35000,
-        400000,
-        5000000,
-        60000000
+        1,
+        8,
+        47,
+        260,
+        1400,
+        7800,
+        44000,
+        260000
     ],
 
     price: [
@@ -196,40 +167,143 @@ const building = {
 
 const upgrade = {
     name: [
-        "Strong hands",
-        "Genetically modified bamboo"
+        "Hobbist luck",
+        "Getting a hang of it",
+        "Bamboo collecting expert",
+        "Collecting master",
+        "Genetically modified bamboo",
+        "Better fertilizer",
+        "Better use of space",
+        "Open to public",
+        "Wild life",
+        "Monkey business",
+        "Tourism",
+        "Space pact",
+        "System of the dawn",
     ],
     description: [
-        "You're getting better at collecting bamboo. Your clicking power gets doubled",
-        "You found some nice bamboo shoots online. Double income from plants"
+        "You just got into new hobby. We all know beginners luck. Your clicking power gets doubled",
+        "You're getting better with practice. Doubles clicking power",
+        "Your skill is way better. Doubles clicking power",
+        "No one is better in collecting bamboo than you. Quadruples your clicking power.",
+        "You found some nice bamboo shoots online. Double income from plants",
+        "Your friend recommended you a beter fertilizer. Doubles income from plants",
+        "You're able to better manage your garden space. Doubles income from gardens",
+        "You've opened your parks to public. Social bamboo is better bamboo. Doubles income from parks.",
+        "More animals started living in your forest which helps your bamboo. Doubles income from forests.",
+        "Monkeys started investing in your jungles. Doubles income from jungles.",
+        "Toursist start visit your islands and all income goes into bamboo farms. Doubles income from islands.",
+        "Made a pact with other planets to not raid you. Ever. Doubles income from plantes.",
+        "Eating sees as a pastime activity. Doubles toxicity of our city. I mean income form systems."
     ],
     image: [
-        "bamboo.png",
-        "bambooShoot.jpg"
+        "cursor.png",
+        "cursor.png",
+        "cursor.png",
+        "goldencursor.png",
+        "bambooShoot.png",
+        "bambooShoot.png",
+        "plot.png",
+        "garden.png",
+        "park.png",
+        "forest.png",
+        "jungle.png",
+        "island.png",
+        "planet.png",
+        "system.png"
     ],
     type: [
         "click",
-        "building"
+        "click",
+        "click",
+        "click",
+        "building",
+        "building",
+        "building",
+        "building",
+        "building",
+        "building",
+        "building",
+        "building",
+        "building",
+        "building",
     ],
     cost: [
         100,
-        100
+        500,
+        10000,
+        100000,
+        100,
+        500,
+        1000,
+        11000,
+        120000,
+        1300000,
+        14000000,
+        200000000,
+        3300000000,
+        51000000000
     ],
     buildingIndex: [
         0,
-        0
+        0,
+        0,
+        0,
+        0,
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
     ],
     requirement: [
         1,
-        10
+        100,
+        1000,
+        5000,
+        1,
+        10,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
     ],
     bonus: [
         2,
-        2
+        2,
+        2,
+        4,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
     ],
     purchased: [
         false,
-        false
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
     ],
 
     purchase: function(index) {
@@ -257,33 +331,157 @@ const upgrade = {
 const achievement = {
     name: [
         "Clicking rookie",
-        "Plant rookie",
-        "Passive rookie"
+        "Clicking expert",
+        "Clicking master",
+        "Clicking pro",
+        "Clicking god",
+        "First plant",
+        "Casual planter",
+        "Plant expert",
+        "Planting addict",
+        "Planting master",
+        "Planting god",
+        "Bamboo hobbist",
+        "Casual bamboo farmer",
+        "Nice Bamboo ;)",
+        "Bamboo maker",
+        "Bamboo investor",
+        "Bamboo master",
+        "Bamboo king",
+        "Bamboo emperor",
+        "Bamboo god"
+    ],
+    image: [
+        "cursor.png",
+        "cursor.png",
+        "cursor.png",
+        "cursor.png",
+        "goldencursor.png",
+        "bambooshoot.png",
+        "bambooshoot.png",
+        "bambooshoot.png",
+        "bambooshoot.png",
+        "bambooshoot.png",
+        "bambooshoot.png",
+        "bamboosmol.png",
+        "bamboosmol.png",
+        "bamboosmol.png",
+        "bamboosmol.png",
+        "bamboosmol.png",
+        "bamboosmol.png",
+        "bamboosmol.png",
+        "bamboosmol.png",
+        "bamboosmol.png",
     ],
     requirement: [
-        10,
+        100,
+        1000,
+        10000,
+        100000,
+        1000000,
         1,
-        40
+        10,
+        100,
+        1000,
+        10000,
+        100000,
+        1,
+        10,
+        69,
+        100,
+        1000,
+        10000,
+        50000,
+        100000,
+        1000000
     ],
     description: [
-        "You clicked 10 times!",
-        "Got 10 plants",
-        "Earning 1 bamboo per second"
+        "You clicked 100 times!",
+        "You clicked 1000 times!",
+        "You clicked 10000 times!",
+        "You clicked 100000 times!",
+        "You clicked 1000000 times!",
+        "Planted your first bamboo!",
+        "You got 10 bamboo plants",
+        "You got 100 bamboo plants",
+        "You got 1000 bamboo plants",
+        "You got 10000 bamboo plants",
+        "You got 100000 bamboo plants",
+        "Your farm grows 1 bamboo per second!",
+        "You earn 10 bamboo per second",
+        "NICE (You earn 69 bamboo per second",
+        "You earn 100 bamboo per second",
+        "You earn 1000 bamboo per second",
+        "You earn 10000 bamboo per second",
+        "You earn 50000 bamboo per second",
+        "You earn 100000 bamboo per second",
+        "You earn 1000000 bamboo per second",
     ],
     type: [
         "click",
+        "click",
+        "click",
+        "click",
+        "click",
         "building",
-        "income"
+        "building",
+        "building",
+        "building",
+        "building",
+        "building",
+        "income",
+        "income",
+        "income",
+        "income",
+        "income",
+        "income",
+        "income",
+        "income",
+        "income",
     ],
     buildingIndex: [
         0,
         0,
-        0
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
     ],
     achieved: [
         false,
         false,
-        false
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
     ],
     getAchievement: function() {
         for (i=0; i < this.name.length; i++){
@@ -407,6 +605,7 @@ setInterval(() => {
     display.updateUpgrades()
     display.updateScore()
     display.udpateShop()
+    display.updateAchievement()
     achievement.getAchievement()
     saveGame()
 }, 10000)
@@ -417,4 +616,5 @@ window.onload = function(){
     display.udpateShop()
     display.updateScore()
     display.updateUpgrades()
+    display.updateAchievement()
 }
